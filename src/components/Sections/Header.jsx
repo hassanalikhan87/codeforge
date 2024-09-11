c6cd756588c5bee4c30814237f60fd27bccce34d
Hassan Ali Khan <hassanalikhan87@gmail.com>
2024-09-11 12:03:28 +1000

    feat: 001 - Setting up content.

---
+++ src/components/Sections/Header.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import styled from "styled-components";
+// Components
+import FullButton from "../Buttons/FullButton";
+// Assets
+import HeaderImage from "../../assets/img/header-img.png";
+import QuotesIcon from "../../assets/svg/Quotes";
+
+export default function Header() {
+  return (
+    <Wrapper id="home" className="container flexSpaceCenter">
+      <LeftSide className="flexCenter">
+        <div>
+          <h1 className="extraBold font60">Hire the Best Talent.</h1>
+          <HeaderP className="font13 semiBold">
+          We are a leading software company that brings together top-rated experts and latest technologies for developing high-performance end-to-end solutions.
+          </HeaderP>
+          <BtnWrapper>
+            <FullButton title="Get Started" />
+          </BtnWrapper>
+        </div>
+      </LeftSide>
+      <RightSide>
+        <ImageWrapper>
+          <Img className="radius8" src={HeaderImage} alt="office" style={{zIndex: 9}} />
+          <QuoteWrapper className="flexCenter darkBg radius8">
+            <QuotesWrapper>
+              <QuotesIcon />
+            </QuotesWrapper>
+            <div>
+              <p className="font15 whiteColor">
+                <em>Design is a funny word. Some people think design means how it looks. But of course, if you dig deeper, it's really how it works.</em>
+              </p>
+              <p className="font13 orangeColor textRight" style={{marginTop: '10px'}}>Steve Jobs</p>
+            </div>
+          </QuoteWrapper>
+        </ImageWrapper>
+      </RightSide>
+    </Wrapper>
+  );
+}
+
+
+const Wrapper = styled.section`
+  padding-top: 80px;
+  width: 100%;
+  min-height: 840px;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+const LeftSide = styled.div`
+  width: 50%;
+  height: 100%;
+  @media (max-width: 960px) {
+    width: 100%;
+    order: 2;
+    margin: 50px 0;
+    text-align: center;
+  }
+  @media (max-width: 560px) {
+    margin: 80px 0 50px 0;
+  }
+`;
+const RightSide = styled.div`
+  width: 50%;
+  height: 100%;
+  @media (max-width: 960px) {
+    width: 100%;
+    order: 1;
+    margin-top: 30px;
+  }
+`;
+const HeaderP = styled.div`
+  max-width: 470px;
+  padding: 15px 0 50px 0;
+  line-height: 1.5rem;
+  @media (max-width: 960px) {
+    padding: 15px 0 50px 0;
+    text-align: center;
+    max-width: 100%;
+  }
+`;
+const BtnWrapper = styled.div`
+  max-width: 190px;
+  @media (max-width: 960px) {
+    margin: 0 auto;
+  }
+`;
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  z-index: 9;
+  @media (max-width: 960px) {
+    width: 100%;
+    justify-content: center;
+  }
+`;
+const Img = styled.img`
+  @media (max-width: 560px) {
+    width: 80%;
+    height: auto;
+  }
+`;
+const QuoteWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 50px;
+  max-width: 330px;
+  padding: 30px;
+  z-index: 99;
+  @media (max-width: 960px) {
+    left: 20px;
+  }
+  @media (max-width: 560px) {
+    bottom: -50px;
+  }
+`;
+const QuotesWrapper = styled.div`
+  position: absolute;
+  left: -20px;
+  top: -10px;
+`;
